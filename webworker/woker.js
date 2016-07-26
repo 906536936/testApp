e3ca4d994e8afcbd07984e14e395b3c7493f4173
alanzhang(张伦) <alanzhang@futunn.com>
2016-07-26 20:11:28 +0800

    alanzhang001

--- webworker/woker.js
+++ webworker/woker.js
@@ -1,23 +1,31 @@
 var fibonacci = function (n) {
 	// return  n < 2 ? n :arguments.callee(n-1)+arguments.callee(n-2);
 
-    var ss= "A345aeBwtA2qwerC";
-    var ss2= "A345aeBwtA2qwerC";
+    var ss = "A345aeBwtA2qwerC";
+    var ss2 = "A345aeBwtA2qwerC";
 
-    for(var i = 0;i< 20;i++){ss = ss+ss;}
+    for (var i = 0; i < 20; i++) {
+        ss = ss + ss;
+    }
 
     ss2 = ss;
 
-    var s1 =  Date.now();
-    ss.replace(/[A-Z]/g,function(s,index){return index ===0 ?s:  "_" + s.toLowerCase()});
+    var s1 = Date.now();
+    var r1 = ss.replace(/[A-Z]/g, function(s, index) {
+        return index === 0 ? s : "_" + s.toLowerCase()
+    });
     var time1 = Date.now() - s1;
 
     var s2 = Date.now();
-    ss2.substr(0,1) + ss2.substr(1).replace(/[A-Z]/g,function(s,index){return "_" + s.toLowerCase()});
+    var r2 = ss2.substring(0, 1) + ss2.substring(1).replace(/[A-Z]/g, function(s, index) {
+        return "_" + s.toLowerCase()
+    });
 
     var time2 = Date.now() - s2;
 
-    return time1 +"_" +time2;
+    console.log(r1 == r2);
+
+    return time1 + "_" + time2;
 }
 
 onmessage = function (event) {
