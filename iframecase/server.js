8d5cc2383057a3dbe79dca0c94fa6b990e43e523
alanzhang001 <alanzhang001@qq.com>
2019-12-22 16:52:24 +0800

    iframe 分享

--- iframecase/server.js
+++ iframecase/server.js
@@ -9,14 +9,24 @@ const Router = require('koa-router')
 const staticServe = require('koa-static');
 const logger = require('koa-logger');
 const path = require('path');
+const bodyParser = require('koa-body');
 const exec = require('child_process').exec;
 // 配置路由
 let router = new Router();
 
-router.post('/query', async (ctx) => {
-
-}).post('/query-fail', async (ctx) => {
-
+router.post('/report', async (ctx) => {
+    console.log('发现有人攻击');
+    console.log(JSON.stringify(ctx.request.body));
+    ctx.set('Content-Type', 'application/json');
+    ctx.body = {
+        code: '0',
+        error: '前端应该接受不到这个响应'
+    };
+}).get('/report', async (ctx) => {
+    ctx.body = {
+        code: '0',
+        error: 'get'
+    };
 });
 
 // 静态资源目录对于相对入口文件server.js的路径
@@ -24,17 +34,21 @@ const staticPath = path.join(__dirname, './nodeviews');
 // 装载路由
 app.use(logger())
     .use(UserAgent)
+    .use(bodyParser({
+    }))
     .use(router.routes())
     .use(router.allowedMethods())
     .use(staticServe(staticPath, {
         index: 'jshack.html',
         setHeaders: function (res, path,stat) {
             if (path.indexOf('futu5xframe') > -1){
-                // res.setHeader('X-Frame-Options', 'sameorigin');
-                res.setHeader('X-Frame-Options', 'allow-from http://webtouch.com:3000/');
+                res.setHeader('X-Frame-Options', 'sameorigin');
+                // res.setHeader('X-Frame-Options', 'allow-from http://webtouch.com:3000/');
             } else if (path.indexOf('futu5csp') > -1){
                 res.setHeader('Content-Security-Policy', 'frame-ancestors https://*.futunn.com https://*.futu5.com http://webtouch.com/');
                 // res.setHeader('Content-Security-Policy', 'frame-ancestors https://*.futunn.com https://*.futu5.com http://webtouch.com:3000/');
+            } else if (path.indexOf('report') > -1){
+                res.setHeader('Content-Security-Policy', 'frame-ancestors https://*.futunn.com https://*.futu5.com http://webtouch.com/;report-uri http://webtouch.com:3000/report');
             }
 
         }
